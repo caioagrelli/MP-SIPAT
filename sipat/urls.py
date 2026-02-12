6ddd8b5d83dff69ccd3e554ed9cb23bc0f255f35
caioagrelli <caiooagrelli@hotmail.com>
2026-02-12 04:03:15 -0300

    bonoiteeeee

--- sipat/urls.py
+++ sipat/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("home/", homepage, name="homepage"),
+    path('home/', homepage, name='homepage'),
+    path('bensconsumo/', bensconsumo, name='bensconsumo'),
+    path('benspermanentes/', benspermanentes, name='benspermanentes'),
+    path('setores/', setores, name='setores')
 ]
--- sipat/views.py
+++ sipat/views.py
@@ -4,10 +4,22 @@ from django.shortcuts import redirect
 
 def root(request):
     if request.user.is_authenticated:
-        return redirect("/admin/") # página quando o usuario estiver logado:
+        return redirect("homepage") # página quando o usuario estiver logado:
     return redirect("login")  #caso não manda pra tela de login
 
 @login_required
 def homepage(request):
     return render(request, 'sipat/homepage.html')
     
+@login_required
+def bensconsumo(request):
+    return render(request, 'sipat/consumo.html')
+
+@login_required
+def benspermanentes(request):
+    return render(request, 'sipat/permanentes.html')
+
+@login_required
+def setores(request):
+    return render(request, 'sipat/setores.html')
+
