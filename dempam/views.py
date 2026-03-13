77e8e378695bde7f7fa3d0edc60c67873778c3af
Caio Agrelli <caiooagrelli@hotmail.com>
2026-03-13 10:49:56 -0300

    feat: desing base

--- dempam/views.py
+++ dempam/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 
 def root(request):
     if request.user.is_authenticated:
-        return redirect('homepage') # página quando o usuario estiver logado:
+        return redirect('home') # página quando o usuario estiver logado:
     return redirect('login')  #caso não manda pra tela de login
 
 @login_required
@@ -15,3 +15,7 @@ def home(request):
 @login_required
 def homepage(request):
     return render(request, 'dempam/homepage.html')
+
+@login_required
+def demands(request):
+    return render(request, 'dempam/demands.html')
