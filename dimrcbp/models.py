c55a6af635549e20e23629fa5f496655edaae9f0
caioagrelli <caiooagrelli@hotmail.com>
2026-04-16 16:38:38 -0300

    feat(models): add class BensPermanentes

--- dimrcbp/models.py
+++ dimrcbp/models.py
@@ -484,3 +484,67 @@ class MovimentacoesPermanentes(models.Model):
     def __str__(self):
        return str(self.codigo)            
 
+
+class BensPermanentes(models.Model):
+    tombo = models.CharField(max_length=20, unique=True, verbose_name='Tombo')
+    
+    description = models.TextField(verbose_name='Descrição')
+    
+    mark = models.CharField(max_length=60, verbose_name='Marca/Fabricante')
+    
+    model = models.CharField(max_length=60, verbose_name='Modelo')
+    
+    n_series = models.CharField(max_length=30, verbose_name='Número de Série')
+    
+    acquisition_date = models.DateField(verbose_name='Data da Aquisição')
+    
+    value = models.DecimalField(max_digits=12, decimal_places=2, verbose_name='Valor Unitário')
+        
+    photo = models.ImageField(upload_to=caminho_benspermanentes, blank=True, null=True, verbose_name='Imagem do Bem')  
+    
+    state = models.CharField(max_length=20, choices=EstadoConservacao.choices, blank=True, null=True, verbose_name='Estado de Conservação')
+    
+    situacion = models.CharField(max_length=20, choices=SituacaoFisica.choices, blank=True, null=True, verbose_name='Situação Física')
+    
+    entry_method = models.CharField(max_length=15, blank=True, default='Compra', verbose_name='Forma de Ingresso')
+    
+    n_empenho = models.CharField(max_length=20, blank=True, default='S/Número', verbose_name='Número do Empenho')
+    
+    n_process = models.CharField(max_length=20, blank=True, default='S/Número', verbose_name='Número do Processo')
+    
+    modality = models.CharField(max_length=25, blank=True, default='Pregão Eletrônico', verbose_name='Modalidade')
+    
+    supllier = BRCNPJField(blank=True, null=True, verbose_name='Fornecedor')
+    
+    
+    class Meta:
+        verbose_name = 'Bem Permanente'
+        verbose_name_plural = 'Bens Permanentes'
+        
+    def __str__(self):
+        return f'Tombo: {self.tombo} - Descrição: {self.description[:30]}'
+    
+    
+class Description(models.Model):
+    description = models.TextField(verbose_name='Descrição')
+    
+    group = models.CharField(max_length=30, blank=True, null=True, verbose_name='Grupo')
+    
+    type = models.CharField(max_length=30, blank=True, null=True, verbose_name='Tipo')
+    
+    classification = models.CharField(max_length=30, blank=True, null=True, verbose_name='Classificação')
+    
+    subclassification = models.CharField(max_length=30, blank=True, null=True, verbose_name='Subclassificação')
+    
+    btu_hp = models.CharField(max_length=30, blank=True, null=True, verbose_name='BTU/HP')
+    
+    size = models.CharField(max_length=30, blank=True, null=True, verbose_name='Tamanho')
+    
+    color = models.CharField(max_length=30, blank=True, null=True, verbose_name='Cor')
+    
+    class Meta:
+        verbose_name = 'Descrição'
+        verbose_name_plural = 'Descrições'
+        
+    def __str__(self):
+        return self.description[:30]    
